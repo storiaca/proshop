cf1fed918ec82cd1269a229e1cd0811eb07ae9ba
Aleksandar Ristic <storiaca@gmail.com>
2020-10-27 17:54:20 +0100

    Add star in rating

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Rating = ({value, text}) => {
+const Rating = ({ value, text }) => {
   return (
-    <div>
-      <p>Rating</p>
+    <div className="rating">
+      <span>
+        <i
+          className={
+            value >= 1
+              ? "fas fa-star"
+              : value >= 0.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Rating
+export default Rating;
