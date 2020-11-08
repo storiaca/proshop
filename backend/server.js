f1ffb8f5f762640e3ca574b89cfb0159edae59a2
Aleksandar Ristic <storiaca@gmail.com>
2020-11-08 13:07:37 +0100

    ES Modules in Node.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
 dotenv.config();
 
@@ -20,5 +20,8 @@ app.get("/api/products/:id", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-  
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
