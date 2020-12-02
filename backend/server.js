1b09feb922ed07d860ad6bc29b3b0e7ca07893c4
Aleksandar Ristic <storiaca@gmail.com>
2020-12-02 10:08:19 +0100

    Custom Error handling finish

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
@@ -16,20 +18,9 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
+app.use(notFound);
 
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-});
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
