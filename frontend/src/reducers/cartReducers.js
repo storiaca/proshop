70101b55d4f319033a89748961db2711c7f0cde8
Aleksandar Ristic <storiaca@gmail.com>
2021-04-21 16:53:06 +0200

    Create cart reducer

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,13 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+
+      const existItem = state.cartItems.find((x) => x.product === item.product);
+
+    default:
+      return state;
+  }
+};
