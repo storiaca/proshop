9c8b88d88cbd588060abab5f1175c6ab0dc9ee60
Software/Nation <storiaca@gmail.com>
2021-05-24 16:07:08 +0200

    Cart Screen start

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -9,6 +9,7 @@ import {
   Form,
   Button,
   Card,
+  ListGroupItem,
 } from "react-bootstrap";
 import Message from "../components/Message";
 
@@ -40,7 +41,34 @@ const CartScreen = ({ match, location, history }) => {
             Your cart is empty <Link to="/">Go Back</Link>
           </Message>
         ) : (
-          <ListGroup variant="flush"></ListGroup>
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={qty}
+                      onChange={(e) => dispatch(addToCart(item.product))}
+                    >
+                      {[...Array(product.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
         )}
       </Col>
       <Col md={2}></Col>
