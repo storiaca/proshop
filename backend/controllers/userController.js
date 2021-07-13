881f2d16dff292b3abdd4c4982665b5f2a60c179
Software/Nation <storiaca@gmail.com>
2021-07-13 16:21:12 +0200

    Custom Authentication middleware start

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,4 +24,24 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Get user profile
+// @route  GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
 export { authUser };
