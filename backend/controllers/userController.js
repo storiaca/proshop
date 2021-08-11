7cf9a913df19dd15d73ba5d8982c3c0fd899d0e7
Software/Nation <storiaca@gmail.com>
2021-08-11 16:16:08 +0200

    User registration and passsword encryption start

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,28 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Register a new user
+// @route  POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
 // @desc   Get user profile
 // @route  GET /api/users/profile
 // @access Private
