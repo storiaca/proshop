7f1a8ffc2efd0637be7f911b5e48fb52913db3ac
Software/Nation <storiaca@gmail.com>
2021-08-13 13:45:43 +0200

    Create user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,8 +32,19 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const userExists = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
-    res.json({
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -41,8 +52,7 @@ const registerUser = asyncHandler(async (req, res) => {
       token: generateToken(user._id),
     });
   } else {
-    res.status(401);
-    throw new Error("Invalid email or password");
+    res.status(400);
   }
 });
 
