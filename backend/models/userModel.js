59cea898dbe9b03fb32474b45bf7f1b656000deb
Software/Nation <storiaca@gmail.com>
2021-08-18 15:30:46 +0200

    Create and import registerUser fix

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,6 +27,9 @@ const userSchema = mongoose.Schema(
 );
 
 userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
